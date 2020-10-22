ce7992d3c59a04d33e1480cb835788e1cf32ad25
Francesco <francesco.savignano96@gmail.com>
2020-10-22 17:08:28 +0200

    aggiunta funzione playGame() per cominciare il gioco

--- main.js
+++ main.js
@@ -9,10 +9,8 @@ var difficulty = 0;
 //i numeri rnd devono assumere un valore tra min e max
 var min = 1;
 var max;
-//se bomb_found diventa true, l'utente ha perso
-var bomb_found = false;
-//punteggio
-var score = 0;
+//Punteggio
+var user_score;
 
 //tre livelli di difficoltà
 max = selectDifficulty(difficulty);
@@ -27,41 +25,12 @@ var bombs = bombsGenerator(min, max);
 console.log('Mine: ' + bombs);
 
 //scelta dell'utente
-var user_number;
+var user_choice;
 
-//numeri che l'utente indovina e che non potra reinserire
-var selected_numbers = [];
-
-//ripeti fino a che l'utente non trova una bomba o raggiunge il punteggio massimo
-do {
-
-    //richiesta di un numero all'utente tra min e max(con controllo del valore inserito)
-    do {
-        user_number = parseInt(prompt('Inserire un numero tra ' + min + ' e ' + max));
-
-        if (user_number < 1 || user_number > 100 || isNaN(user_number)) {
-            alert('Inserimento sbagliato..');
-        }else if (selected_numbers.includes(user_number)) {
-            alert('numero gia inserito!');
-        }
-
-    } while (user_number < 1 || user_number > 100 || isNaN(user_number) || (selected_numbers.includes(user_number)));
-    console.log('User Number: ' + user_number);
-
-    //se l'utente trova un numero bomba perde
-    if(bombs.includes(user_number)){
-        bomb_found = true;
-        console.log('Mi dispaice hai perso..');
-    }
-    //altrimenti aumenta il punteggio di uno e aggiunge il numero a quelli che non può reinserire
-    else{
-        score += 1;
-        selected_numbers.push(user_number);
-    }
-
-} while (bomb_found == false && score != (max_score));
+//inizia il gioco
+user_score = playGame(user_choice);
 //stampa punteggio
-console.log('Punteggio: ' + score + '/' + max_score);
+console.log('Punteggio: ' + user_score + '/' + max_score);
 
 //FUNZIONI
 //funzione che restituisce il range delle mine generate
@@ -92,4 +61,47 @@ function bombsGenerator(min, max){
         }
     }
     return rnd_numbers;
+}
+
+//fa partire il gioco
+function playGame(user_number){
+    //se bomb_found diventa true, l'utente ha perso
+    var bomb_found = false;
+    //numeri che l'utente indovina e che non potra reinserire
+    var selected_numbers = [];
+    //punteggio
+    var score = 0;
+
+    //inizia il gioco
+    console.log('GAME STARTED!');
+
+    //ripeti fino a che l'utente non trova una bomba o raggiunge il punteggio massimo
+    do {
+
+        //richiesta di un numero all'utente tra min e max(con controllo del valore inserito)
+        do {
+            user_number = parseInt(prompt('Inserire un numero tra ' + min + ' e ' + max));
+
+            if (user_number < 1 || user_number > 100 || isNaN(user_number)) {
+                alert('Inserimento sbagliato..');
+            }else if (selected_numbers.includes(user_number)) {
+                alert('numero gia inserito!');
+            }
+
+        } while (user_number < 1 || user_number > 100 || isNaN(user_number) || (selected_numbers.includes(user_number)));
+        console.log('User Number: ' + user_number);
+
+        //se l'utente trova un numero bomba perde
+        if(bombs.includes(user_number)){
+            bomb_found = true;
+            console.log('Mi dispaice hai perso..');
+        }
+        //altrimenti aumenta il punteggio di uno e aggiunge il numero a quelli che non può reinserire
+        else{
+            score += 1;
+            selected_numbers.push(user_number);
+        }
+
+    } while (bomb_found == false && score != (max_score));
+    return score;
 }
